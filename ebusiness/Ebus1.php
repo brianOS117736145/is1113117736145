9580efa9fe7811121f6b4217cceed8228eb12a27
Brian O'Sullivan <117736145@umail.ucc.ie>
2018-03-08 21:28:53 +0000

    testing

--- ebusiness/Ebus1.php
+++ ebusiness/Ebus1.php
@@ -9,7 +9,16 @@
     </head>
     
     <body>
-        
+          <nav id="mainnav" class="fluid ">
+      <div id="minMenu" class="fluid listitems hide_tablet hide_desktop" >Menu</div>
+      <ul id="navlist" class="fluid fluidList navlist">
+        <li class="fluid listitems"><a href="#" class="fluid listitems thispage">Home</a></li>
+        <li class="fluid listitems"><a href="cv/cv_page1.html">CV</a></li>
+        <li class="fluid listitems"><a href="Interests/sports.html">Interests</a></li>
+        <li class="fluid listitems"><a href="ebusiness/Ebus1.php">Ebus</a></li>
+        <li class="fluid listitems"><a href="cloud/cloud.html">About Cloud</a></li>
+      </ul>
+    </nav>
         <h4>Select a product</h4>
         
         <br/>
--- ebusiness/Ebus2.php
+++ ebusiness/Ebus2.php
@@ -12,6 +12,16 @@ session_start();
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     </head>
     <body>
+          <nav id="mainnav" class="fluid ">
+      <div id="minMenu" class="fluid listitems hide_tablet hide_desktop" >Menu</div>
+      <ul id="navlist" class="fluid fluidList navlist">
+        <li class="fluid listitems"><a href="#" class="fluid listitems thispage">Home</a></li>
+        <li class="fluid listitems"><a href="cv/cv_page1.html">CV</a></li>
+        <li class="fluid listitems"><a href="Interests/sports.html">Interests</a></li>
+        <li class="fluid listitems"><a href="ebusiness/Ebus1.php">Ebus</a></li>
+        <li class="fluid listitems"><a href="cloud/cloud.html">About Cloud</a></li>
+      </ul>
+    </nav>
         <h4>Please enter your payment details</h4>
         
         <form action="Ebus3.php" method="POST">
